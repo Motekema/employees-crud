ce946bc40eb1c34fee2322cbb591e68ef298f9d2
Motekema <motekema@gmail.com>
2025-03-07 00:33:54 +0200

    Add error handling for missing MONGODB_URI environment variable

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -3,6 +3,13 @@ import bcrypt from "bcryptjs";
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
 const client = new MongoClient(uri);
 
 export default async function handler(
