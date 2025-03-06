662f65c435655950c5802ee3c449dc8fead3b311
Motekema <motekema@gmail.com>
2025-03-06 21:01:29 +0200

    Added

--- models/Employee.ts
+++ models/Employee.ts
@@ -1,16 +1,16 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 interface IEmployee extends Document {
-  firstname: string;
-  lastname: string;
+  firstName: string;
+  lastName: string;
   email: string;
   phone: string;
   role: string;
 }
 
 const EmployeeSchema: Schema = new Schema({
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
   role: { type: String, required: true },
